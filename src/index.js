328515cca46e556757c3cbf4e32742095fdc9f09
github username <github email address>
2020-06-05 05:51:11 -0400

    updated index.js with postfetchpin function, rendering pin data

--- src/index.js
+++ src/index.js
@@ -36,10 +36,33 @@ function createPinFormHandler(e) {
     const descriptionInput = document.querySelector('#input-description').value
     const brandInput = document.querySelector('#brands').value
     const brandId = parseInt(brandInput)
-    // postfetchPin(titleInput, image_urlInput, descriptionInput, brandInput)
+    const userInput = document.querySelector('#input-user_id').value
+    const userId = parseInt(userInput)
+    postfetchPin(titleInput, image_urlInput, descriptionInput, brandInput, userInput)
 
 }
 
-function postfetchPin(title, image_url, description, brand_id) {
-    console.log(title, image_url, description, brand_id)
+function postfetchPin(title, image_url, description, brand_id, user_id) {
+    const pinBodyData = {title, image_url, description, brand_id, user_id}
+    fetch(pinsEndpoint, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(pinBodyData)
+      })
+      .then(response => response.json())
+      .then(pin => {
+        const pinData = pin.data
+            const pinsLayout = 
+                `<div data-id=${pin.id}>
+                <h4>${pinData.attributes.title}</h4>
+                <img src=${pinData.image_url} width="800" height="600">
+                <h4>${pinData.description}</h4> 
+                <h4>${pinData.brand.name}</h4>
+                <h4>${pinData.user.id}</h4>  
+            <br>
+                <button data-id=${pin.id}>Edit</button>
+                </div>`;
+
+        document.querySelector("#pins-container").innerHTML += pinsLayout
+})
 }
