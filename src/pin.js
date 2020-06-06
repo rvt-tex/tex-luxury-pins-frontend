5b7e346b92aea149c77cc1e8f8537f908b2a2548
github username <github email address>
2020-06-06 02:16:02 -0400

    created a class constructor for pin

--- src/pin.js
+++ src/pin.js
@@ -1,3 +1,11 @@
 class Pin {
 
+    constructor(id, pinAttributes) {
+        this.id = id;
+        this.title = pinAttributes.title;
+        this.image_url = pinAttributes.image_url;
+        this.description = pinAttributes.description;
+        this.brand = pinAttributes.brand;
+        this.user = pinAttributes.user
+    }
 }
