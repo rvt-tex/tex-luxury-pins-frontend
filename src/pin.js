0ad53bbb1c3098c76ebc5e8fdd672936e3b0d0d9
github username <github email address>
2020-06-12 16:22:21 -0400

    deleted user files

--- src/pin.js
+++ src/pin.js
@@ -6,14 +6,12 @@ class Pin {
         this.image_url = pinAttributes.image_url;
         this.description = pinAttributes.description;
         this.brand = pinAttributes.brand;
-        this.user = pinAttributes.user
         Pin.all.push(this);
     }
 
     renderPinCard() {
         return (
         `<div data-id=${this.id}>
-                <h4>${this.user.name}  @${this.user.email}</h4>
                 <h4>Brand: ${this.brand.name}</h4>
                 <h4>Title: ${this.title}</h4>
                 <img src=${this.image_url} width="600" height="400">
